e1ab9df0b107bb41141693bed264fa09264f3a3d
Harry Craven <48776040+HarryCravDev@users.noreply.github.com>
2020-11-07 14:55:06 +0000

    Update - Two Sum Algorithm

--- JavaScript_Algorithms.js
+++ JavaScript_Algorithms.js
@@ -4,3 +4,18 @@ const camelCase = string => {
         return (char === char.toUpperCase()) ? ` ${char}` : char
     }).join('')
 }
+
+//  @desc   Two sum - Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+const twoSum = (nums, target) => {
+    const previousValues = {};
+    for(let i = 0; i < nums.length; i++){
+        const currentNumber = nums[i];
+        const neededNumber = target - currentNumber;
+        const index1 = previousValues[neededNumber];
+        if(index1 != null){
+            return [index1, i]
+        } else {
+            previousValues[currentNumber] = i;
+        }
+    }
+};
